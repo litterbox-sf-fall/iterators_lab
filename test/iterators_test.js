18996c19aeb5203f39ee7cfe6a79f631c7ed5352
Tim Licata <tim@timothylicata.com>
2014-10-06 20:04:00 -0700

    implementation and test for min

--- test/iterators_test.js
+++ test/iterators_test.js
@@ -15,4 +15,15 @@ describe('Iterators', function () {
       expect(Iterators.max(myArr)).to.equal(-Infinity);
     });
   });
+
+  describe('#min', function () {
+    it('should return the minimum in an array', function () {
+      expect(Iterators.min([1,2,3,4,5])).to.equal(1);
+      expect(Iterators.min([-1,0,3,4,5])).to.equal(-1);
+      expect(Iterators.min([0,3,4,5])).to.equal(0);
+    });
+    it('should return Infinity for an empty array', function () {
+      expect(Iterators.min([])).to.equal(Infinity);
+    });
+  });
 });
